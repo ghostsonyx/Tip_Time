706ec256999166bdfa0dabda136ae6474d8b74f0
ghostsonyx <66806782+ghostsonyx@users.noreply.github.com>
2021-12-19 21:26:16 -0600

    Add Other tip percentage input

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -2,8 +2,14 @@ package com.example.tiptime
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.view.View
+import android.widget.RadioButton
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.core.widget.addTextChangedListener
 import com.example.tiptime.databinding.ActivityMainBinding
+import com.google.android.material.textfield.TextInputLayout
 import java.text.NumberFormat
 import kotlin.math.ceil
 
@@ -11,14 +17,32 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private var tipPercentage = 0.00
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Check if cost of service has been added to enable calculate button
-        binding.costOfService.addTextChangedListener {
-            binding.calculateButton.isEnabled = binding.costOfService.text.isNotEmpty()
+        // Check if cost of service changes to enable calculate button
+        binding.costOfServiceEditText.addTextChangedListener {
+            isCalculateButtonAvailable()
+        }
+
+        // check if other tip changes to enable calculate button
+        binding.optionOtherEditText.addTextChangedListener {
+            isCalculateButtonAvailable()
+        }
+
+        binding.tipOptions.setOnCheckedChangeListener { radioGroup, checkedId ->
+            when(binding.tipOptions.checkedRadioButtonId) {
+                R.id.option_twenty_percent -> binding.optionOtherEditText.isEnabled = false
+                R.id.option_eighteen_percent -> binding.optionOtherEditText.isEnabled = false
+                R.id.option_fifteen_percent -> binding.optionOtherEditText.isEnabled = false
+                R.id.option_other_percent -> binding.optionOtherEditText.isEnabled = true
+            }
+
+            isCalculateButtonAvailable()
         }
 
         binding.calculateButton.setOnClickListener {
@@ -27,9 +51,16 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculateTip() {
-        val costOfService = binding.costOfService.text.toString().toDoubleOrNull()
+        // validate cost of service input
+        val costOfService = binding.costOfServiceEditText.text.toString().toDoubleOrNull()
 
         if (costOfService == null || costOfService == 0.00) {
+            displayTip(0.00)
+            return
+        }
+
+        // validate tip
+        if (!isTipPercentageKnown()) {
             displayTip(0.00)
             return
         }
@@ -38,6 +69,7 @@ class MainActivity : AppCompatActivity() {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             R.id.option_fifteen_percent -> 0.15
+            R.id.option_other_percent -> binding.optionOtherEditText.text.toString().toDouble() / 100.00
             else -> 0.00
         }
 
@@ -52,5 +84,27 @@ class MainActivity : AppCompatActivity() {
     private fun displayTip(tip: Double) {
         val formattedTip = NumberFormat.getCurrencyInstance().format(tip)
         binding.tipResult.text = getString(R.string.tip_amount, formattedTip)
+    }
+
+    private fun isTipPercentageKnown(): Boolean {
+        val tipPercentage = when(binding.tipOptions.checkedRadioButtonId) {
+            R.id.option_twenty_percent -> 0.20
+            R.id.option_eighteen_percent -> 0.18
+            R.id.option_fifteen_percent -> 0.15
+            R.id.option_other_percent -> {
+                if (binding.optionOtherEditText.text?.isNotEmpty() == true) {
+                    binding.optionOtherEditText.text.toString().toDouble()
+                } else {
+                    return false
+                }
+            }
+            else -> null
+        }
+
+        return (tipPercentage is Double)
+    }
+
+    private fun isCalculateButtonAvailable() {
+        binding.calculateButton.isEnabled = binding.costOfServiceEditText.text?.isNotEmpty() == true && isTipPercentageKnown()
     }
 }
